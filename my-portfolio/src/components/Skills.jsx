b7e62bcb881884b4ad173de2015e3d6a2adbfea8
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-01-09 22:40:59 -0500

    adding skiils with no image and text

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -13,11 +13,11 @@ import aws from "../assets/aws.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="bg-[#0a192f]  text-gray-300">
+    <div name="skills" className=" w-full h-screen bg-[#0a192f]  text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
-            Experience
+            Skills
           </p>
           <p className="py-4">
             These are some of the skills I have used in my project and work
@@ -57,6 +57,41 @@ const Skills = () => {
           <div className="w-150px shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={expressjs} alt="java icon" />
             <p className="px-4">Express JS</p>
+          </div>
+        </div>
+
+        {/* skills in text */}
+
+        <div className="pt-12">
+          <p className="py-4">
+            These are some of the skills I have used in my project and work
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-6 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">Git</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">Maven</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">Jenkins</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">Elasticsearch</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">Pulumi</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">GCP</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">REST APIs</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] flex items-center justify-center w-32 h-16">
+            <p className="px-4 text-center">Postman</p>
           </div>
         </div>
       </div>
