40787b97b06330f53c44e1308e5e47e9db982f4b
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-01-11 22:44:21 -0500

    resume download

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/aws.png";
 import { FaBars, FaTimes, FaHome, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail, HiOutlineDocument } from "react-icons/hi";
 import { Link } from "react-scroll";
-import { myPDF } from "Aishwarya_V_resume.pdf";
+// import { myPDF } from "../../src/Aish";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
@@ -120,7 +120,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href={myPDF}
+              href="/AishwaryaVresume.pdf"
               download="aishwarya_v_resume.pdf"
             >
               Resume
