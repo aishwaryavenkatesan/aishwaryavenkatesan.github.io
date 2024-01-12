0c8a1548864bf7a2f5c1cb24f879feab1ed43979
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-01-11 22:29:44 -0500

    minor changes

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -120,7 +120,8 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://github.com/aishwaryavenkatesan/aishwaryavenkatesan.github.io/blob/main/Aishwarya_V_resume.pdf"
+              download={Aishwarya_V_resume.pdf}
             >
               Resume
               <HiOutlineDocument size={30} />
