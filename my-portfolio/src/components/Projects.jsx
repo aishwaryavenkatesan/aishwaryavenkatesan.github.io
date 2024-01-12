e64907decab9a0e8b5ebeab718cf943944d58aed
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-01-11 19:48:49 -0500

    adding code for projects page with image and skills

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import youtubeClone from "../assets/youtubeClone.jpg";
 
 const Projects = () => {
   return (
@@ -8,19 +9,40 @@ const Projects = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Projects
           </p>
-          <p>Check out the projects I have worked on so far</p>
+          <p className="py-6">Check out the projects I have worked on so far</p>
         </div>
 
-        <div>
-          <div>
-            <div>
-              <span></span>
-              <div>
+        {/* container */}
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* project element */}
+          <div
+            style={{ backgroundImage: `url(${youtubeClone})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <span className="text-2xl sm:text-[20px] font bold text-white tracking-wider">
+                {" "}
+                Serverless Cloud Application
+              </span>
+
+              {/* <p className="text-center">Tech Stack</p>
+              <div className="flex flex-wrap justify-center">
+                <p className="px-4">Git</p>
+                <p className="px-4">HTML</p>
+                <p className="px-4">CSS</p>
+                <p className="px-4">JavaScript</p>
+                <p className="px-4">HTML</p>
+                <p className="px-4">CSS</p>
+                <p className="px-4">JavaScript</p>
+              </div> */}
+
+              <div className="pt-8 text-center">
                 <a href="/">
-                  <button></button>
-                </a>
-                <a href="/">
-                  <button></button>
+                  <button className="text-center rounded-lg px-4 py-2 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
