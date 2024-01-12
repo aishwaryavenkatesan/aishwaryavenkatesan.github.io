ed39f870a51b5af64cfda638f7811315130fda1c
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2024-01-11 23:33:36 -0500

    resume link in github pages

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/AishwaryaVresume.pdf"
+              href="https://github.com/aishwaryavenkatesan/aishwaryavenkatesan.github.io/blob/main/AishwaryaVresume.pdf"
               download="aishwarya_v_resume.pdf"
             >
               Resume
